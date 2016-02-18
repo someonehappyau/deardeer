759895c83f6b9ffb2e21d93b8ef0e1548315ae58
someonehappy <someonehappyau@gmail.com>
2016-02-19 08:01:24 +1100

    adding log system

--- server/app.js
+++ server/app.js
@@ -2,8 +2,31 @@ var express=require('express')
 var bodyParser=require('body-parser')
 var path=require('path');
 
+var logger=require('morgan');
+var FileStreamRotator=require('file-stream-rotator');
+var fs=require('fs');
+
 var app=express()
 
+var logDirectory = __dirname + '/log';
+
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+// create a rotating write stream
+var accessLogStream = FileStreamRotator.getStream({
+  filename: logDirectory + '/access-%DATE%.log',
+  frequency: 'daily',
+  verbose: false,
+  date_format: 'YYYYMMDD'
+});
+
+app.enable('trust proxy');
+
+// setup the logger
+app.use(logger('combined', {stream: accessLogStream}));
+
+// set view engine
 app.set('views',[__dirname+'/library/views'])
 app.set('view engine','jade')
 
